15accfbf190f5bad7ed9fa1e499b4f15b87bc115
agile <agile@macbook.local>
2010-12-28 22:45:51 +0900

    display formatted

--- misc/pdf.py
+++ misc/pdf.py
@@ -1,12 +1,19 @@
 # -*- coding: utf8 -*-
-from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 from reportlab.pdfbase import pdfmetrics
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Frame, PageTemplate, NextPageTemplate
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Frame, PageTemplate, NextPageTemplate
+from reportlab.platypus.flowables import HRFlowable
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.pdfbase.ttfonts import TTFont
+#from reportlab.pdfbase.cidfonts import UnicodeCIDFont
+
+import yaml
 
 class MySimpleDocTemplate(SimpleDocTemplate):
-    pdfmetrics.registerFont(UnicodeCIDFont("HeiseiKakuGo-W5"))
-    PStyle = ParagraphStyle(name='Normal', fontName="HeiseiKakuGo-W5", fontSize=8, leading=10, borderWidth=1)
+    #pdfmetrics.registerFont(UnicodeCIDFont("HeiseiKakuGo-W5"))
+    pdfmetrics.registerFont(TTFont('IPA', 'ipag-mona.ttf'))
+
+    PStyle = ParagraphStyle(name='Normal', fontName="IPA", fontSize=6, leading=7, borderWidth=1, wordWrap='CJK')
+    #PStyle = ParagraphStyle(name='Normal', fontName="HeiseiKakuGo-W5", fontSize=6, leading=7, borderWidth=1, wordWrap='CJK')
     pageCount = 1
 
     def __init__(self,filename):
@@ -18,10 +25,10 @@ class MySimpleDocTemplate(SimpleDocTemplate):
 
         self.addPageTemplates([
             PageTemplate(id="p1", frames=[
-                Frame(x1=10, y1=10, width=247,  height=390, showBoundary=1, topPadding=15)
-                ,Frame(x1=10, y1=430, width=247, height=390, showBoundary=1, topPadding=15)
-                ,Frame(x1=270, y1=10, width=247, height=390, showBoundary=1, topPadding=15)
-                ,Frame(x1=270, y1=430, width=247, height=390, showBoundary=1, topPadding=15)
+                Frame(x1=10, y1=10,    width=260, height=405, showBoundary=1, topPadding=15)
+                ,Frame(x1=10, y1=430,  width=260, height=405, showBoundary=1, topPadding=15)
+                ,Frame(x1=270, y1=10,  width=260, height=405, showBoundary=1, topPadding=15)
+                ,Frame(x1=270, y1=430, width=260, height=405, showBoundary=1, topPadding=15)
             ])
         ])
 
@@ -40,11 +47,15 @@ def go():
     doc = MySimpleDocTemplate("reportlab_japanese.pdf")
     Story = []
     Story.append(NextPageTemplate('p1'))
+    Story.append(HRFlowable(width='110%'))
 
-    for i in range(5):
-        bogustext = (u"\rこんにちわこんにちわ！！") * 100
-        Story.append(Paragraph(bogustext, doc.PStyle))
-        Story.append(Spacer(0, 10))
+    data = yaml.load(open('moge.yaml').read().decode('utf8'))
+
+    for d in data:
+        tweet = d['text']
+        Story.append(Paragraph(tweet, doc.PStyle))
+        Story.append(Paragraph( d['created_at'] + str(d['id']), doc.PStyle))
+        Story.append(HRFlowable(width='110%'))
 
     doc.build(Story)
 
