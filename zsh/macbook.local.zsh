4edaeb059f5564be4e533343b1e4e68eba96e865
celeron1ghz <celeron1ghz@gmail.com>
2015-03-20 11:11:05 +0900

    removed olds

--- zsh/macbook.local.zsh
+++
@@ -1,5 +0,0 @@
-alias ls='ls -vG'
-export PATH="/usr/local/bin:/usr/local/mysql/bin:/opt/local/bin:/opt/local/sbin:$PATH"
-
-#export WORKON_HOME=$HOME/.virtualenvs
-#source /usr/local/bin/virtualenvwrapper.sh
