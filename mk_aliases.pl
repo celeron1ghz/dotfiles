d5b905a359d3f4b051b5b3ae8d061386dda4758f
celeron1ghz <celeron1ghz@gmail.com>
2015-04-25 13:31:54 +0900

    fixed mk_aliases.pl not work in other directory

--- mk_aliases.pl
+++ mk_aliases.pl
@@ -1,37 +1,39 @@
 #!/usr/bin/perl
 use strict;
 use File::Find;
+use File::Basename;
+use FindBin;
 
-my $HOME = $ENV{HOME};
-my $PWD  = $ENV{PWD};
+my $HOME    = $ENV{HOME};
+my $REPODIR = $FindBin::Bin;
+my @files;
 
-## aliasing dotfiles
 find(sub{
-	next if $_ eq '.';
-	next if $_ eq '..';
-	next if $_ eq '.git';
-	next unless /^\./;
+    next if $_ eq '.';
+    next if $_ eq '..';
+    next if $_ eq '.git';
+    next unless /^\./;
+    push @files, $File::Find::name;
+}, $REPODIR);
 
-	my $from   = "$PWD/$_";
-	my $target = "$HOME/$_";
+for my $file (@files)   {
+    my $aliase_to = $ENV{HOME} . '/' . basename($file);
 
-    unless ( -e $target )   {
-	    symlink $from => $target or die "fail to make $target: $!";
-	    print "aliased $from -> $target\n";
-    }
-}, '.');
-
-## aliasing normal files
-my %alias = (
-    "$PWD/tmux/mytheme.sh" => "$HOME/tmux-powerline/themes/mytheme.sh"
-);
-
-while ( my($from,$target) = each %alias )   {
-    if (-l $target) {
-        warn "symlink $target already exist";
+    if (-l $aliase_to)  {
+        warn "exist alias  : $aliase_to\n";
         next;
     }
 
-    symlink $from => $target or die "fail to make $target: $!";
-    print "aliased $from -> $target\n";
+    if (-e $aliase_to)  {
+        warn "exist file   : $aliase_to\n";
+        next;
+    }
+
+    symlink $file => $aliase_to or die "fail to make $aliase_to: $!";
+        warn "create alias : $file -> $aliase_to\n";
 }
+
+__END__
+my %alias = (
+    "$PWD/tmux/mytheme.sh" => "$HOME/tmux-powerline/themes/mytheme.sh"
+);
