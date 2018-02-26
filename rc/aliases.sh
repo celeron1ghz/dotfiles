677335071889d4fd9fc273883fe58d4a61901abe
celeron1ghz <celeron1ghz@gmail.com>
2018-02-26 01:24:19 +0000

    using in darwin

--- rc/aliases.sh
+++ rc/aliases.sh
@@ -3,13 +3,13 @@ alias cls='clear'
 alias gd='git diff'
 alias gs='git status'
 alias gl='git log'
-alias vim='mvim -v'
 
 case "${OSTYPE}" in
   darwin*)
     alias ls="ls -G"
     alias ll="ls -lG"
     alias la="ls -laG"
+    alias vim='mvim -v'
     ;;
   linux*)
     alias ls='ls --color'
