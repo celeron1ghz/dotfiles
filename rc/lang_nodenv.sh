1846e62d05feea21510ff25a3c9497f7f7118a25
celeron1ghz <celeron1ghz@gmail.com>
2020-07-18 22:36:01 +0900

    add nodenv setup

---
+++ rc/lang_nodenv.sh
@@ -0,0 +1,13 @@
+##
+## node.js
+##
+NODE_DIR=$HOME/.nodenv
+export PATH="$PATH:$NODE_DIR/bin"
+
+if [ "`command -v nodenv`" = "" ]; then
+  git clone https://github.com/nodenv/nodenv.git $NODE_DIR
+  mkdir -p $NODE_DIR/plugins
+  git clone https://github.com/nodenv/node-build.git $NODE_DIR/plugins/node-build
+fi
+
+eval "$(nodenv init -)"
