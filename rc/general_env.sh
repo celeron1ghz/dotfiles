2ded36d8de9a8f61523148214959b467a29fc9b5
celeron1ghz <celeron1ghz@gmail.com>
2020-07-27 15:47:27 +0900

    fix direnv setup

--- rc/general_env.sh
+++ rc/general_env.sh
@@ -5,7 +5,7 @@ HISTFILE=$HOME/.zsh_history
 HISTSIZE=100000
 SAVEHIST=100000
 
-if [ `which direnv` != "" ]; then
+if [ "`command -v direnv`" != "" ]; then
   echo "direnv init..."
   eval "$(direnv hook zsh)"
   direnv allow
