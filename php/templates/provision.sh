0be021fbdb108bf5b8e7f016d6d6256cfbcd6c1c
celeron1ghz <celeron1ghz@gmail.com>
2020-07-27 17:19:11 +0900

    add database provisioner

---
+++ php/templates/provision.sh
@@ -0,0 +1,13 @@
+
+DATA1={{ lib_dir }}/sql/master_data.txt
+DATA2={{ lib_dir }}/sql/master_data2.txt
+DDL={{ lib_dir }}/sql/createtable_posgresql.sql
+
+echo "data: $DATA1, $DATA2"
+echo "ddl:  $DDL"
+
+echo "DROP DATABASE {{ pg_database }}; CREATE DATABASE {{ pg_database }};" | psql -U postgres
+
+psql -U {{ pg_username }} {{ pg_database }} < $DDL
+psql -U {{ pg_username }} {{ pg_database }} < $DATA1
+psql -U {{ pg_username }} {{ pg_database }} < $DATA2
