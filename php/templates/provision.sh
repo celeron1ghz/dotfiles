fd230d5ade339294ac9e9818407e15832f7fcadc
celeron1ghz <celeron1ghz@gmail.com>
2020-07-27 17:44:41 +0900

    oops

--- php/templates/provision.sh
+++ php/templates/provision.sh
@@ -1,7 +1,7 @@
 
-DATA1={{ lib_dir }}/sql/master_data.txt
-DATA2={{ lib_dir }}/sql/master_data2.txt
-DDL={{ lib_dir }}/sql/createtable_posgresql.sql
+DATA1={{ lib_dir }}/{{ project }}/sql/master_data.txt
+DATA2={{ lib_dir }}/{{ project }}/sql/master_data2.txt
+DDL={{ lib_dir }}/{{ project }}/sql/createtable_posgresql.sql
 
 echo "data: $DATA1, $DATA2"
 echo "ddl:  $DDL"
