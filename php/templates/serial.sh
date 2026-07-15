0f40defd27815723be6077a21316f68dcf210dc2
celeron1ghz <celeron1ghz@gmail.com>
2026-07-15 22:29:50 +0900

    remove unused

--- php/templates/serial.sh
+++
@@ -1,6 +0,0 @@
-DATA={{ lib_dir }}/{{ project }}/sql/serial_sql.sql
-
-echo "data: $DATA"
-
-psql -U {{ pg_username }} {{ pg_database }} < $DATA
-
--- php/templates/start.sh
+++
@@ -1,5 +0,0 @@
-sudo service apache2 start
-sudo service postgresql start
-
-cd ~/dotfiles/php
-ansible-playbook -vv init.yaml
