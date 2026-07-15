06136b230b19aa6b3d38c3696d62d4247327e90d
celeron1ghz <celeron1ghz@gmail.com>
2026-07-15 22:17:58 +0900

    remove vscode setting

--- mk_aliases.pl
+++ mk_aliases.pl
@@ -14,6 +14,7 @@ find(sub{
     next if $_ eq '.git';
     next if $_ eq '.gitignore';
     next unless /^\./;
+    next if $File::Find::dir ne $REPODIR;
     push @files, $File::Find::name;
 }, $REPODIR);
 
@@ -35,27 +36,3 @@ for my $file (@files)   {
     warn "create alias : $file -> $aliase_to\n";
 }
 
-if ($^O eq 'darwin') {
-    my @macfiles;
-    my $repoPath = "$REPODIR/vscode/";
-    my $vscodeSettingPath   = "${HOME}/Library/Application Support/Code/";
-
-    find(sub{
-        next unless -f $_;
-        push @macfiles, $File::Find::name;
-    }, $repoPath);
-
-    for my $from (@macfiles) {
-        (my $base = $from) =~ s/$repoPath//;
-        my $aliase_to = "$vscodeSettingPath$base";
-
-        if (-e $aliase_to) {
-            warn "exist mac alias  : $aliase_to\n";
-            next;
-        }
-
-        symlink $from => $aliase_to or die "fail to make $aliase_to: $!";
-
-        warn "create mac alias : $from -> $aliase_to\n";
-    }
-}
